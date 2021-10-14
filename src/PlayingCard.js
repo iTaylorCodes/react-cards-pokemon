e70d75c385d9716e5b9362836d36e4217d28d244
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-10-13 23:48:59 -0700

    Created useAxios hook/Added delete cards functionality/formatted data

--- src/PlayingCard.js
+++ src/PlayingCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import useFlip from "./hooks";
+import { useFlip } from "./hooks";
 import backOfCard from "./back.png";
 import "./PlayingCard.css";
 
--- src/hooks.js
+++ src/hooks.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { v4 as uuid } from "uuid";
 import React, { useState } from "react";
 
 const useFlip = () => {
@@ -8,4 +10,17 @@ const useFlip = () => {
   return [isFacingUp, flipCard];
 };
 
-export default useFlip;
+const useAxios = (base_url) => {
+  const [respData, setRespData] = useState([]);
+
+  const fetchNewData = async (formatter = (data) => data, restOfUrl = "") => {
+    const resp = await axios.get(`${base_url}${restOfUrl}`);
+    setRespData((initialData) => [...initialData, formatter(resp.data)]);
+  };
+
+  const clearResponses = () => setRespData([]);
+
+  return [respData, fetchNewData, clearResponses];
+};
+
+export { useFlip, useAxios };
